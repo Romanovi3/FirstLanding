ef0f17503230fd2c8fa185e4c615e14faee7540f
Vladislav Romanovich Bauer <bauerxvii@gmail.com>
2023-09-15 09:56:29 +0700

    another one commit. added new styles, some funtcions and new section. fixed bugs.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // stick Header
+
 let header = document.getElementById("header");
 let sticky = header.offsetTop;
 window.onscroll = function() {myFunction()};
-
 
 function myFunction() {
   if (window.scrollY >= sticky) {
@@ -11,3 +11,22 @@ function myFunction() {
     header.classList.remove("stickyHeader");
   }
 }
+
+// Email form alert
+
+let emailFormBtn = document.getElementById ('efBtn')
+
+ 
+
+
+emailFormBtn.addEventListener ('click', function(){
+  let emailFormName = document.getElementById ('emailFormName').value
+  let emailFormEmail = document.getElementById ('emailFormEmail').value
+
+  if ( emailFormEmail && emailFormName !== 0){
+      alert('Thank you dear ,'+ emailFormName +' , we will definitely contact you at the earliest opportunity!')
+    }
+  else{  
+    alert ('Enter your information!')
+  }
+});
